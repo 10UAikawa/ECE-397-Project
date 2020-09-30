62ae636f48b03f24df3aa2c331a5eac275162ec8
10UAikawa <71509442+10UAikawa@users.noreply.github.com>
2020-09-30 16:42:46 -0500

    Update GcodeAuto.py

--- GcodeAuto.py
+++ GcodeAuto.py
@@ -1,4 +1,7 @@
 import pyautogui, time
+from pyfirmata import ArduinoMega
+from pyfirmata import SERVO
+import time
 
 time.sleep(2)#give time to close out of program
 
@@ -25,9 +28,26 @@ def Send():
     cord = pyautogui.locateCenterOnScreen('Send.png')
     time.sleep(2)#giving time to find on screen before clicking
     pyautogui.click(cord)
-
-
-
     
+#Closes the gripper/vaccum
+def MvServoClose(ServoNum):
+    #moving servo from 0 to 180 degrees
+    for i in range(180):
+        board.digital[ServoNum].write(i)   
+        time.sleep(0.01)   #the speed of the servo .01 seconds is a good speed
+
+#Opens the gripper/vaccum
+def MyServoOpen(ServoNum):
+    #moving servo from 180 to 0 degrees
+    for i in range(180,0,-1):
+        board.digital[ServoNum].write(i)
+        time.sleep(0.01)  #the speed of the servo .01 seconds is a good speed
+    
+    
+
+board = ArduinoMega('COM7')  #ArduinoMega connecting to computers COM port
+board.digital[6].mode = SERVO   #servo object, (change pin Number depending on which servo)
+
+
 
 
