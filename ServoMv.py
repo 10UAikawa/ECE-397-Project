dc04e1cd24b30d72180c622080081c403defaec3
10UAikawa <71509442+10UAikawa@users.noreply.github.com>
2020-11-06 12:36:55 -0600

    Update ServoMv.py

--- ServoMv.py
+++ ServoMv.py
@@ -5,18 +5,19 @@ import time
 
 
 board = ArduinoMega('COM7')  #ArduinoMega connecting to computers COM port
+pin = 6
+range_val_in_degrees = 160
+board.digital[pin].mode = SERVO   #servo object
 
-board.digital[6].mode = SERVO   #servo object
-
-#moving servo from 0 to 180 degrees
-for i in range(180):
-    board.digital[6].write(i)   
+#moving servo from 0 to 160 degrees
+for i in range(range_val_in_degrees):
+    board.digital[pin].write(i)   
     time.sleep(0.01)   #the speed of the servo .01 seconds is a good speed
 
 
-#moving servo from 180 to 0 degrees
-for i in range(180,0,-1):
-    board.digital[6].write(i)
+#moving servo from 160 to 0 degrees
+for i in range(range_val_in_degrees,0,-1):
+    board.digital[pin].write(i)
     time.sleep(0.01)  #the speed of the servo .01 seconds is a good speed
 
 
